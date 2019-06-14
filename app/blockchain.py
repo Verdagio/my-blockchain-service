57b77b35fce152f8404054f9230deb3998b459e8
Verdagio <dverdagio@gmail.com>
2019-06-14 18:58:44 +0100

    adding comments to some of the more complex functions

--- app/blockchain.py
+++ app/blockchain.py
@@ -6,15 +6,30 @@ from functools import reduce
 
 MINER_REWARD = 10
 
+
 class Blockchain:
-   
+
     def __init__(self, host_id):
-        GENESIS_BLOCK = Block(0,'',[],100,0)
+        GENESIS_BLOCK = Block(0, '', [], 100, 0)
         self.chain = [GENESIS_BLOCK]
         self.open_transactions = []
         self.host = host_id
 
-    def create_transaction(self, sender, receiver, amount): 
+    def create_transaction(self, sender, receiver, amount):
+        '''
+        Creates a transaction between users, this transaction is then
+        validated. If the user has enough funds the transaction is added
+        onto a list of open transactions.
+
+        Args:
+            sender: The user who will send funds
+            receiver: The user who will receive the funds
+            amount: The amount of funds to be sent
+
+        Returns:
+            True if the transaction was successful.
+            False if the transaction fails.
+        '''
         tx = Transaction(sender, receiver, amount)
         if Validator.validate_transaction(tx, self.get_balance):
             self.__open_transactions.append(tx)
@@ -22,34 +37,55 @@ class Blockchain:
         return False
 
     def mine_block(self, miner):
+        '''
+        Creates a block compiling of:
+        1 - the current index
+        2 - a hash of the last block in the chain
+        3 - the list of open transactions
+        4 - the proof
+
+        Adds the block to the chain, then resets the open transactions.
+
+        Args:
+            miner: the user which is mining the block
+        
+        Returns:
+            True if the process was successful
+        '''
         hashed_block = hash_utils.hash_block(self.__chain[-1])
         copy_open_tx = self.__open_transactions[:]
         mined_tx = Transaction('MINED', miner, MINER_REWARD)
         copy_open_tx.append(mined_tx)
-        block = Block(len(self.__chain), hashed_block, copy_open_tx, self.proof_of_work())
+        block = Block(len(self.__chain), hashed_block,
+                      copy_open_tx, self.proof_of_work())
         self.__chain.append(block)
         self.__open_transactions = []
         return True
 
     def proof_of_work(self):
+
         last_block = self.__chain[-1]
         last_hash = hash_utils.hash_block(last_block)
         proof = 0
         while not Validator.validate_proof(self.__open_transactions, last_hash, proof):
-            proof +=1
+            proof += 1
         return proof
 
-    def get_balance(self): 
-        tx_sender = [[tx.amount for tx in block.transactions if tx.sender == self.host] for block in self.__chain]
-        open_tx_sender = [tx.amount for tx in  self.__open_transactions if tx.sender == self.host]
+    def get_balance(self):
+        tx_sender = [[tx.amount for tx in block.transactions if tx.sender == self.host]
+                     for block in self.__chain]
+
+        open_tx_sender = [
+            tx.amount for tx in self.__open_transactions if tx.sender == self.host] 
         tx_sender.append(open_tx_sender)
-        tx_receiver = [[tx.amount for tx in block.transactions  if tx.receiver == self.host] for block in self.__chain]
+        tx_receiver = [[tx.amount for tx in block.transactions if tx.receiver == self.host]
+                       for block in self.__chain]
         amt_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                            if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+                          if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
         amt_rec = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                            if len(tx_amt) > 0 else tx_sum + 0, tx_receiver, 0)
+                         if len(tx_amt) > 0 else tx_sum + 0, tx_receiver, 0)
         return amt_rec - amt_sent
-    
+
     def get_last_block(self):
         return None if len(self.__chain) < 1 else self.__chain[-1]
 
@@ -60,7 +96,7 @@ class Blockchain:
     @chain.setter
     def chain(self, val):
         self.__chain = val
-    
+
     @property
     def open_transactions(self):
         return self.__open_transactions[:]
--- app/utils/validator.py
+++ app/utils/validator.py
@@ -6,17 +6,57 @@ class Validator:
 
     @staticmethod
     def validate_proof(transactions, last_hash, proof):
+        '''
+        Encodes:
+        1 - the list of open transactions
+        2 - the hash of the previous block
+        3 - a number 0 - n
+
+        into bytes,  creates a hash from this, and validates the hash
+
+        Args:
+            transactions: list of open transactions
+            last_hash: the hash of the previous block
+            proof: a non negative whole number
+
+        Returns:
+            True if the validators hash first 2 characters are: '00'
+
+        '''
         validator = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
         validator_hash = hash_utils.hash_string(validator)
         return validator_hash[0:2] == '00'
     
     @staticmethod
     def validate_transaction(transaction, get_balance):
+        '''
+        Checks that the user has enough funds to complete the transaction
+
+        Args:
+            transaction: an object which contains the sender, receiver, and amount
+            get_balance: a function which checks the users balance
+        Returns:
+            True if the user has enough funds
+            False if the user does not have enough funds
+        '''
         balance = get_balance()
         return balance >= transaction.amount
 
     @classmethod
     def is_valid_chain(cls, blockchain):
+        '''
+        Validates that the blockchain itself is pure.
+        Done by comparing the hashes and proofs of each 
+        block to ensure correctness.
+
+        Args:  
+            blockchain: the chain of blocks to validate
+
+        Returns:
+            True if the blockchain is indeed valid 
+            False if it is not.
+
+        '''
         for (i, block) in enumerate(blockchain):
             if i == 0:
                 continue  # No need to verify the the genesis block
@@ -28,6 +68,16 @@ class Validator:
 
     @classmethod
     def validate_open_transactions(cls, open_transactions):
+        '''
+        Validates all open transactions
+
+        Args:
+            open_transactions: list of pending transactions
+
+        Returns:
+            True if all are valid
+            False if not
+        '''
         return all(list(map(cls.validate_transaction, open_transactions)))
     
 
